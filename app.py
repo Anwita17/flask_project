984ecd20db811ae9cce411e6dbec276170911c38
Anwita17 <mitun.i.rock@gmail.com>
2021-05-13 01:38:30 +0530

    second commit

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/view_details')
 def details():
     return render_template("view_details.html")
+
 
 @app.route('/upload')
 def upload():
     return render_template("upload.html")
 
+
 @app.route('/send_mail')
 def send_mail():
     return render_template("send_mail.html")
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-    
