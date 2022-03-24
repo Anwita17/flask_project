c326c7ebf3650671065cbb60deef729e49b9b8ac
Anwita17 <55680982+Anwita17@users.noreply.github.com>
2022-03-24 22:32:19 +0530

    Update mail_config.py

--- mail_config.py
+++ mail_config.py
@@ -3,5 +3,5 @@ class mail_conf:
     MAIL_PORT =  587
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    MAIL_USERNAME = 'anonymousanwitashobhit@outlook.com'
-    MAIL_PASSWORD = 'Anonymous22'
+    MAIL_USERNAME = 'urmailid@outlook.com'
+    MAIL_PASSWORD = 'xxxxxxxxxxx'
